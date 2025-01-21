2c51e4e9d4bb0818bb36f971871d5ce692237c0b
SUNIL GADHE <89225052+sunilgadhe@users.noreply.github.com>
2025-01-21 14:54:42 +0000

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "my_access_key"
+    secret_key = "my_secret_key"
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-xxx"
+    instance_type = "t2.micro"
+}
